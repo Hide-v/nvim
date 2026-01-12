a24d6a0cf7605950d2af721e99b74a848abd5024
Hide-v <vcmingri19@outlook.com>
2026-01-12 18:19:03 +0800

    change blink.lua about keymap and menu

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,3 +1,8 @@
+local function has_words_before()
+  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
   "saghen/blink.cmp",
   dependencies = { "rafamadriz/friendly-snippets" },
@@ -8,10 +13,37 @@ return {
   ---@type blink.cmp.Config
   opts = {
     keymap = {
-      preset = "enter",
-      ["<C-y>"] = { "select_and_accept" },
-      ["<C-d>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-u>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-N>"] = { "select_next", "show" },
+      ["<C-P>"] = { "select_prev", "show" },
+      ["<C-J>"] = { "select_next", "fallback" },
+      ["<C-K>"] = { "select_prev", "fallback" },
+      ["<C-U>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-D>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = {
+        "select_next",
+        "snippet_forward",
+        function(cmp)
+          if has_words_before() or vim.api.nvim_get_mode().mode == "c" then
+            return cmp.show()
+          end
+        end,
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        "select_prev",
+        "snippet_backward",
+        function(cmp)
+          if vim.api.nvim_get_mode().mode == "c" then
+            return cmp.show()
+          end
+        end,
+        "fallback",
+      },
     },
 
     appearance = {
@@ -22,6 +54,30 @@ return {
       documentation = {
         auto_show = false,
         auto_show_delay_ms = 500,
+      },
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                return kind_icon
+              end,
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+            kind = {
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+          },
+        },
       },
     },
 
