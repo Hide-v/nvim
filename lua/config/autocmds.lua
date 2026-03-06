98698301c5457f14ade4be6f750c49c9ecfa73b9
Hide-v <vcmingri19@outlook.com>
2026-03-06 16:01:35 +0800

    feat(autocmds): add diagnostic underline highlighting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function set_diagnostic_underline()
+  local error_color = vim.api.nvim_get_hl(0, { name = "DiagnosticError" }).fg
+  local warn_color = vim.api.nvim_get_hl(0, { name = "DiagnosticWarn" }).fg
+  local info_color = vim.api.nvim_get_hl(0, { name = "DiagnosticInfo" }).fg
+  local hint_color = vim.api.nvim_get_hl(0, { name = "DiagnosticHint" }).fg
+
+  vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline = true, sp = error_color })
+  vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { underline = true, sp = warn_color })
+  vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { underline = true, sp = info_color })
+  vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true, sp = hint_color })
+end
+
+set_diagnostic_underline()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = set_diagnostic_underline,
+})
