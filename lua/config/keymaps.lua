7960b8437ed6d4bc3e73ce31650731d6a53c95d3
Hide-v <vcmingri19@outlook.com>
2026-04-18 23:57:22 +0800

    feat(keymaps): add scroll with center keymaps
    Add <C-d> and <C-u> to scroll 10 lines and center the view

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<C-d>", "10jzz", { desc = "Scroll down 10 lines + center" })
+map("n", "<C-u>", "10kzz", { desc = "Scroll up 10 lines + center" })
